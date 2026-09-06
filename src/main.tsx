63afe0602e9be93af38bad0cb77d6c9d6945cc82
incircledev <incircledev@gmail.com>
2026-09-06 22:57:44 +0800

    Add Google Analytics 4 with measurement ID G-4ZZ79CB412. (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { I18nProvider } from '@/components/i18n-provider';
 import App from './App';
+import { initAnalytics } from './analytics';
 import './index.css';
+
+initAnalytics();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
